c9941f91d95b0ee6bdfd60190deb2e7cb2455422
Minira Samadova <samadova.minira@gmail.com>
2019-10-26 22:14:59 +0900

    made loading cat gif to show

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      fanfics: null,
+      keywords: null,
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.isLoading ? <img alt="loading_cat" src="loading_cat.gif"></img> : null
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
