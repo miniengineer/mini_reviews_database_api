6e8601fac6fd82a4429b2ade024b65c6e182bcc2
Minira Samadova <samadova.minira@gmail.com>
2019-11-01 09:49:17 +0900

    changed to reviews app, created migration file

---
+++ server/db/migrations/20191101083011_create-reviews-table.js
@@ -0,0 +1,21 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('reviews', (table) => {
+    table.increments().index().notNullable(); //auto-incrementing index
+
+    table.string('name').index().notNullable(); //reviewer's name
+
+    table.string('reviewTitle').index().notNullable(); //title of the review
+
+    table.string('product').index().notNullable(); //product being reviewed
+
+    table.text('review_text').index().notNullable(); //actual review
+
+    table.string('rating').index(); //rating of the product
+
+    table.timestamp('created_at').defaultTo(knex.fn.now()); //review date
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('reviews');
+};
--- server/knexfile.js
+++ server/knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection:'postgres://localhost/hpfanfiction',
+    connection:'postgres://localhost/reviews',
     migrations: {
       directory: './db/migrations'
     },
